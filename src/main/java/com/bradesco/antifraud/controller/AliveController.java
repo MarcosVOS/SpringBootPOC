cf008dee0d722f7c703e6dac513d2c1f4f126f26
Marcos <70212363+MarcosVOS@users.noreply.github.com>
2025-05-23 01:25:38 +0000

    swagger

--- src/main/java/com/bradesco/antifraud/controller/AliveController.java
+++ src/main/java/com/bradesco/antifraud/controller/AliveController.java
@@ -10,4 +10,43 @@ public class AliveController {
 	public String index() {
 		return "the application is ok";
 	}
+
+	@GetMapping("/one")
+	public String index1() {
+		return "the application is ok";
+	}
+
+	@GetMapping("/two")
+	public String index2() {
+		return "the application is ok";
+	}
+
+	@GetMapping("/three")
+	public String index3() {
+		return "the application is ok";
+	}
+	@GetMapping("/four")
+	public String index4() {
+		return "the application is ok";
+	}
+
+	@GetMapping("/five")
+	public String index5() {
+		return "the application is ok";
+	}
+
+	@GetMapping("/six")
+	public String index6() {
+		return "the application is ok";
+	}
+
+	@GetMapping("/seven")
+	public String index7() {
+		return "the application is ok";
+	}
+
+	@GetMapping("/eight")
+	public String index8() {
+		return "the application is ok";
+	}
 }
